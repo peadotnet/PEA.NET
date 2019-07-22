8f89f29e9354e0dc81373f269d141077b0d58156
bacskaiz <bacskaiz77@gmail.com>
2019-07-22 17:31:54 +0200

    Sorted subset chromosome: section eliminating mutation

--- src/PEA/PEA/Chromosome/Implementation/SortedSubset/EliminateSectionMutation.cs
+++ src/PEA/PEA/Chromosome/Implementation/SortedSubset/EliminateSectionMutation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Http.Headers;
 using Pea.Core;
 
 namespace Pea.Chromosome.Implementation.SortedSubset
@@ -19,39 +18,30 @@ namespace Pea.Chromosome.Implementation.SortedSubset
             SortedSubsetChromosome clone;
 
             int retryCount = ParameterSet.GetInt(ParameterNames.FailedMutationRetryCount);
-            while (true)
+
+            clone = chromosome.DeepClone();
+            var sectionToEliminate = Random.GetInt(0, clone.Sections.Length);
+            for (int g = 0; g < clone.Sections[sectionToEliminate].Length; g++)
             {
-                int replaced = 0;
-                clone = chromosome.DeepClone();
-                var sectionToEliminate = Random.GetInt(0, clone.Sections.Length);
-                for (int g = 0; g < clone.Sections[sectionToEliminate].Length; g++)
+                var geneValue = clone.Sections[sectionToEliminate][g];
+                var replaceCount = retryCount;
+
+                while (true)
                 {
-                    var geneValue = clone.Sections[sectionToEliminate][g];
-                    var replaceCount = ParameterSet.GetInt(ParameterNames.FailedMutationRetryCount);
+                    var targetSection = Random.GetIntWithTabu(0, clone.Sections.Length, sectionToEliminate);
+                    var targetPosition = FindNewGenePosition(clone.Sections[targetSection], geneValue);
 
-                    while (true)
+                    if (!ConflictDetectedWithLeftNeighbor(clone.Sections[targetSection], targetPosition, geneValue)
+                        && !ConflictDetectedWithRightNeighbor(clone.Sections[targetSection], targetPosition, geneValue))
                     {
-                        var targetSection = Random.GetIntWithTabu(0, clone.Sections.Length, sectionToEliminate);
-                        var targetPosition = FindNewGenePosition(clone.Sections[targetSection], geneValue);
-
-                        if (!ConflictDetectedWithLeftNeighbor(clone.Sections[targetSection], targetPosition, geneValue)
-                            && !ConflictDetectedWithRightNeighbor(clone.Sections[targetSection], targetPosition, geneValue))
-                        {
-                            //TODO: simplify this method parameter set! (or make an alternative one)
-                            InsertGenes(clone, targetSection, targetPosition, new int[] {geneValue}, 0, 1);
-
-
-                            DeleteGenesFromSection(clone, sectionToEliminate, g, 1);
-                            replaced++;
-                            break;
-                        }
-
-                        if (replaceCount-- < 0) break;
+                        //TODO: simplify this method parameter set! (or make an alternative one)
+                        InsertGenes(clone, targetSection, targetPosition, new int[] {geneValue}, 0, 1);
+                        DeleteGenesFromSection(clone, sectionToEliminate, g, 1);
+                        break;
                     }
+
+                    if (replaceCount-- < 0) break;
                 }
-
-                if (replaced == clone.Sections[sectionToEliminate].Length || retryCount-- < 0) break;
-
             }
 
             return clone;
